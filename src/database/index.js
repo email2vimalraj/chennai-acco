3e9a6efa944da56a7d15dd440764071c40fd805d
VimalRaj Selvam <email2vimalraj@gmail.com>
2018-02-04 23:31:12 +0530

    Added mLab mongo sbx

--- src/database/index.js
+++ src/database/index.js
@@ -6,7 +6,12 @@ const connectToDB = async () => {
   const mongoose = new Mongoose();
   mongoose.Promise = global.Promise;
 
-  await mongoose.connect('mongodb://localhost/chennai-acco-db');
+  let connectionString = 'mongodb://vselvam:vselvam@ds123728.mlab.com:23728/heroku_9mmbw63g';
+  if (process.env.NODE_ENV === 'local') {
+    connectionString = 'mongodb://localhost/chennai-acco-db';
+  }
+
+  await mongoose.connect(connectionString);
   connection = mongoose;
 };
 
